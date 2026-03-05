3b7b90cc9aa5cb2f66005ae048181881211df07a
Nathan Edwards <edwardsnj@gmail.com>
2026-03-05 15:34:02 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 ${PYTHON:-python3.12} -m venv .venv
-.venv/bin/python -m pip -r requirements.txt
+.venv/bin/python -m pip install -r requirements.txt
 ln -s ../PyGly/pygly
 ln -s ../PyGly/scripts/glyres.py
-./glyres.py GlyTouCanNoCache allcrossrefs | fgrep kegg > kegg.txt
-./glyres.py PubChemDownload allchebigtc > gtc2chebi.pubchem.txt
+./glyres.py GlyTouCanNoCache allcrossrefs | fgrep kegg_glycan | awk '{print $3,$1}' > kegg.txt
+./glyres.py PubChemDownload allchebigtc | awk '{print $2,$1}'> gtc2chebi.pubchem.txt
 
 
