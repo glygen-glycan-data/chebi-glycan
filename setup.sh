c62e21f39ff68628b3d525b4dd67ba0ac12ffbf5
Nathan Edwards <edwardsnj@gmail.com>
2026-03-05 15:40:41 -0500

    add ontology downloads

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ ${PYTHON:-python3.12} -m venv .venv
 .venv/bin/python -m pip install -r requirements.txt
 ln -s ../PyGly/pygly
 ln -s ../PyGly/scripts/glyres.py
+wget -O chebi.obo.gz 'https://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.obo.gz'
+wget -O GNOme.obo 'https://purl.obolibrary.org/obo/gno.obo'
 ./glyres.py GlyTouCanNoCache allcrossrefs | fgrep kegg_glycan | awk '{print $3,$1}' > kegg.txt
 ./glyres.py PubChemDownload allchebigtc | awk '{print $2,$1}'> gtc2chebi.pubchem.txt
 
