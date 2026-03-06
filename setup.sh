11c3d313bf86c4316e98626f199e8ab832e23463
Nathan Edwards <edwardsnj@gmail.com>
2026-03-06 09:18:14 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-
+set -x
 if [ ! -d .venv ]; then
     ${PYTHON:-python3.12} -m venv .venv
-    ./.venv/bin/python -m pip install -r requirements.txt
+    .venv/bin/python -m pip install -r requirements.txt
 fi
 if [ ! -f chebi.obo.gz ]; then
     wget -O chebi.obo.gz 'https://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.obo.gz'
@@ -14,7 +14,9 @@ if [ ! -d ${PYGLY:-../PyGly} ]; then
     echo "Please checkout the glygen-glycan-data/PyGly repository from GitHub in `dirname ${PWD}` or set PYGLY appropriately" 1>&2
     exit 1
 fi
-ln -s ${PYGLY:-../PyGly}/pygly
+if [ ! -d pygly ]; then
+    ln -s ${PYGLY:-../PyGly}/pygly
+fi
 if [ ! -f kegg.txt ]; then
     ${PYGLY:-../PyGly}/scripts/glyres.py GlyTouCanNoCache allcrossrefs | fgrep kegg_glycan | awk '{print $3,$1}' > kegg.txt
 fi
